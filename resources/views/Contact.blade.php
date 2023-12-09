d4b7d655e4648f419aa842f954803b806dd07c65
HannaYanko <annayanko04@gmail.com>
2023-12-09 13:58:02 +0200

    Laravel

---
+++ resources/views/Contact.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .container {
+            margin-top: 50px;
+        }
+
+        h1 {
+            color:  #ff0000;
+        }
+
+        p {
+            font-size: 30px;
+        }
+    </style>
+    <title>Сторінка Янько</title>
+</head>
+<body>
+
+<div class="container">
+    <h1>Моя перша у житті Laravel-сторінка:)</h1>
+    <p>Тут розповідатиму про мистецькі заходи столиці</p>
+</div>
+</body>
+</html>
